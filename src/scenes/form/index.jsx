475f90159cb70f9129cf261bf471a46aea93ffad
TMH <tmhkma18@gmail.com>
2023-08-03 11:37:09 +0700

    0

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -1,5 +1,5 @@
 import { Box, Button, TextField } from "@mui/material";
-import { Formik } from "formik";
+
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
@@ -10,7 +10,19 @@ const Form = ({ permission, handleClose, setTableUpdate }) => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
   const navigate = useNavigate();
   const [permDDList, setPermDDList] = useState([]);
-  const handleFormSubmit = (values) => {
+  const [values, setValues] = useState({
+    username: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    contact: "",
+    address: "",
+    access:"",
+  })
+
+  
+  const handleFormSubmit = () => {
     axios
       .post("https://node-service-ihr4.onrender.com/signup", values)
       .then((res) => {
@@ -25,11 +37,13 @@ const Form = ({ permission, handleClose, setTableUpdate }) => {
     if (permission.user_create !== 1) {
       navigate("/denyaccess");
     } else {
-      axios.get("https://node-service-ihr4.onrender.com/getPermDDList").then((res) => {
-        //const resultArray = res.data.Result.map((obj) => obj.access_name);
-        setPermDDList(res.data.Result);
-        console.log(permDDList);
-      });
+      axios
+        .get("https://node-service-ihr4.onrender.com/getPermDDList")
+        .then((res) => {
+          //const resultArray = res.data.Result.map((obj) => obj.access_name);
+          setPermDDList(res.data.Result);
+          console.log(permDDList);
+        });
     }
   }, []);
 
@@ -37,166 +51,125 @@ const Form = ({ permission, handleClose, setTableUpdate }) => {
     <Box m="20px">
       <Header title="Tạo mới người dùng" subtitle="Create a New User Profile" />
 
-      <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={checkoutSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <form>
-            <Box
-              display="grid"
-              gap="30px"
-              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-              sx={{
-                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-              }}
+      <form>
+        <Box
+          display="grid"
+          gap="30px"
+          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+          sx={{
+            "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+          }}
+        >
+          <TextField
+            fullWidth
+            variant="filled"
+            type="text"
+            label="Tên người dùng"
+            onChange={(e) => setValues({ ...values, username: e.target.value })}
+            value={values.username}
+            name="username"
+            sx={{ gridColumn: "span 4" }}
+          />
+          <TextField
+            fullWidth
+            variant="filled"
+            type="text"
+            label="Họ thật"
+            onChange={(e) => setValues({ ...values, firstName: e.target.value })}
+            value={values.firstName}
+            name="firstName"
+            sx={{ gridColumn: "span 2" }}
+          />
+          <TextField
+            fullWidth
+            variant="filled"
+            type="text"
+            label="Tên thật"
+            onChange={(e) => setValues({ ...values, lastName: e.target.value })}
+            value={values.lastName}
+            name="lastName"
+            sx={{ gridColumn: "span 2" }}
+          />
+          <TextField
+            fullWidth
+            variant="filled"
+            type="text"
+            label="Email"
+            onChange={(e) => setValues({ ...values, email: e.target.value })}
+            value={values.email}
+            name="email"
+            sx={{ gridColumn: "span 4" }}
+          />
+          <TextField
+            fullWidth
+            variant="filled"
+            type="text"
+            label="Password"
+            onChange={(e) => setValues({ ...values, password: e.target.value })}
+            value={values.password}
+            name="password"
+            sx={{ gridColumn: "span 4" }}
+          />
+          <TextField
+            fullWidth
+            variant="filled"
+            type="text"
+            label="Liên lạc"
+            onChange={(e) => setValues({ ...values, contact: e.target.value })}
+            value={values.contact}
+            name="contact"
+            sx={{ gridColumn: "span 4" }}
+          />
+          <TextField
+            fullWidth
+            variant="filled"
+            type="text"
+            label="Địa chỉ"
+            onChange={(e) => setValues({ ...values, address: e.target.value })}
+            value={values.address}
+            name="address"
+            sx={{ gridColumn: "span 4" }}
+          />
+          <Box>
+            <label htmlFor="email">Quyền truy cập</label>
+            <select
+              fullWidth
+              variant="filled"
+              type="text"
+              label="Quyền truy cập"
+              onChange={(e) => setValues({ ...values, access: e.target.value })}
+              value={values.access}
+              name="access"
+              sx={{ gridColumn: "span 4" }}
             >
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Tên người dùng"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.username}
-                name="username"
-                error={!!touched.username && !!errors.username}
-                helperText={touched.username && errors.username}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Họ thật"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.firstName}
-                name="firstName"
-                error={!!touched.firstName && !!errors.firstName}
-                helperText={touched.firstName && errors.firstName}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Tên thật"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.lastName}
-                name="lastName"
-                error={!!touched.lastName && !!errors.lastName}
-                helperText={touched.lastName && errors.lastName}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Email"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.email}
-                name="email"
-                error={!!touched.email && !!errors.email}
-                helperText={touched.email && errors.email}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Password"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.password}
-                name="password"
-                error={!!touched.password && !!errors.password}
-                helperText={touched.password && errors.password}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Liên lạc"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.contact}
-                name="contact"
-                error={!!touched.contact && !!errors.contact}
-                helperText={touched.contact && errors.contact}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Địa chỉ"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.address}
-                name="address"
-                error={!!touched.address && !!errors.address}
-                helperText={touched.address && errors.address}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <Box>
-              <label htmlFor="email">
-                Quyền truy cập
-              </label>
-              <select
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Quyền truy cập"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.access}
-                name="access"
-                error={!!touched.access && !!errors.access}
-                helperText={touched.access && errors.access}
-                sx={{ gridColumn: "span 4" }}
-              >
-                {permDDList.map(({ access_name }) => (
-                  <option key={access_name} value={access_name}>
-                    {access_name}
-                  </option>
-                ))}
-              </select>
-              </Box>
-            </Box>
-            <Box display="flex" justifyContent="space-between" mt="20px">
-              <Button
-                onClick={handleSubmit}
-                type="submit"
-                color="secondary"
-                variant="contained"
-                sx={{ p: 1 }}
-              >
-                Tạo người dùng mới
-              </Button>
-              <Button
-                onClick={handleClose}
-                color="secondary"
-                variant="contained"
-                sx={{ p: 1 }}
-              >
-                Đóng
-              </Button>
-            </Box>
-          </form>
-        )}
-      </Formik>
+              {permDDList.map(({ access_name }) => (
+                <option key={access_name} value={access_name}>
+                  {access_name}
+                </option>
+              ))}
+            </select>
+          </Box>
+        </Box>
+        <Box display="flex" justifyContent="space-between" mt="20px">
+          <Button
+            onClick={handleSubmit}
+            type="submit"
+            color="secondary"
+            variant="contained"
+            sx={{ p: 1 }}
+          >
+            Tạo người dùng mới
+          </Button>
+          <Button
+            onClick={handleClose}
+            color="secondary"
+            variant="contained"
+            sx={{ p: 1 }}
+          >
+            Đóng
+          </Button>
+        </Box>
+      </form>
     </Box>
   );
 };
