ed0a38ad6b0c936506f8c69d9ed21dd8973b98b3
TMH <tmhkma18@gmail.com>
2023-08-08 11:38:21 +0700

    Email testing

--- src/scenes/mail/index.jsx
+++ src/scenes/mail/index.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import { Button, Box, TextField } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Email = () => {
-    return (
-        <>
-            
-        </>
-    );
+  const [targetMail, setTargetMail] = useState("");
+  const handleEmail = () => {
+    axios
+      .post(" https://node-service-ihr4.onrender.com/mail", targetMail)
+      .then((res) => {
+        if (res.msg === "Success") {
+          console.log("success");
+        } else {
+          console.log("fail");
+        }
+      });
+  };
+  return (
+    <Box>
+      <TextField
+        fullWidth
+        variant="filled"
+        type="text"
+        label="Địa chỉ"
+        onChange={(e) => setTargetMail(e.target.value)}
+        value={targetMail}
+        name="targetMail"
+      />
+
+      <Button onClick={handleEmail}>Email check</Button>
+    </Box>
+  );
 };
 
 export default Email;
