22ce8bb406f950150e21247423f3ce8d7b765f31
TMH <tmhkma18@gmail.com>
2023-08-17 11:15:16 +0700

    0

--- src/scenes/user/profile/index.jsx
+++ src/scenes/user/profile/index.jsx
@@ -1,26 +1,26 @@
 // ** React Imports
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import axios from "axios";
 // ** MUI Imports
-import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
-import Link from '@mui/material/Link'
-import Alert from '@mui/material/Alert'
-import Select from '@mui/material/Select'
-import { styled } from '@mui/material/styles'
-import MenuItem from '@mui/material/MenuItem'
-import TextField from '@mui/material/TextField'
-import Typography from '@mui/material/Typography'
-import InputLabel from '@mui/material/InputLabel'
-import AlertTitle from '@mui/material/AlertTitle'
-import IconButton from '@mui/material/IconButton'
-import CardContent from '@mui/material/CardContent'
-import FormControl from '@mui/material/FormControl'
-import Button from '@mui/material/Button'
-import img from '../../../assets/smug.png';
-import CloseIcon from '@mui/icons-material/Close';
-const UserProfile = ({id}) => {
-    // ** State
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import Alert from "@mui/material/Alert";
+import Select from "@mui/material/Select";
+import { styled } from "@mui/material/styles";
+import MenuItem from "@mui/material/MenuItem";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import InputLabel from "@mui/material/InputLabel";
+import AlertTitle from "@mui/material/AlertTitle";
+import IconButton from "@mui/material/IconButton";
+import CardContent from "@mui/material/CardContent";
+import FormControl from "@mui/material/FormControl";
+import Button from "@mui/material/Button";
+import img from "../../../assets/smug.png";
+import CloseIcon from "@mui/icons-material/Close";
+const UserProfile = ({ id }) => {
+  // ** State
   const [openAlert, setOpenAlert] = useState(true);
   const [data, setData] = useState({
     username: "",
@@ -32,158 +32,210 @@ const UserProfile = ({id}) => {
     contact: "",
     access: "",
   });
-  const onChange = file => {
-    const reader = new FileReader()
-    const { files } = file.target
+  const onChange = (file) => {
+    const reader = new FileReader();
+    const { files } = file.target;
     if (files && files.length !== 0) {
-    //   reader.onload = () => setImgSrc(reader.result)
-      reader.readAsDataURL(files[0])
+      //   reader.onload = () => setImgSrc(reader.result)
+      reader.readAsDataURL(files[0]);
     }
-  }
+  };
 
-  const ImgStyled = styled('img')(({ theme }) => ({
+  const ImgStyled = styled("img")(({ theme }) => ({
     width: 120,
     height: 120,
     marginRight: theme.spacing(6.25),
-    borderRadius: theme.shape.borderRadius
-  }))
-  
+    borderRadius: theme.shape.borderRadius,
+  }));
+
   const ButtonStyled = styled(Button)(({ theme }) => ({
-    [theme.breakpoints.down('sm')]: {
-      width: '100%',
-      textAlign: 'center'
-    }
-  }))
-  
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      textAlign: "center",
+    },
+  }));
+
   const ResetButtonStyled = styled(Button)(({ theme }) => ({
     marginLeft: theme.spacing(4.5),
-    [theme.breakpoints.down('sm')]: {
-      width: '100%',
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
       marginLeft: 0,
-      textAlign: 'center',
-      marginTop: theme.spacing(4)
-    }
-  }))
+      textAlign: "center",
+      marginTop: theme.spacing(4),
+    },
+  }));
 
-  useEffect(() =>{
+  useEffect(() => {
     axios
-    .get(" https://node-service-ihr4.onrender.com/get/" + id)
-    .then((res) => {
-      setData({
-        ...data,
-        username: res.data.Result[0].username,
-        firstname: res.data.Result[0].firstname,
-        lastname: res.data.Result[0].lastname,
-        email: res.data.Result[0].email,
-        address: res.data.Result[0].address,
-        password: "",
-        contact: res.data.Result[0].phone,
-        access: res.data.Result[0].access,
-      });
-    })
-    .catch((err) => console.log(err));
-  }, [])
+      .get(" https://node-service-ihr4.onrender.com/get/" + id)
+      .then((res) => {
+        setData({
+          ...data,
+          username: res.data.Result[0].username,
+          firstname: res.data.Result[0].firstname,
+          lastname: res.data.Result[0].lastname,
+          email: res.data.Result[0].email,
+          address: res.data.Result[0].address,
+          password: "",
+          contact: res.data.Result[0].phone,
+          access: res.data.Result[0].access,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-        <CardContent sx={{ width: "80%", mx: "auto", p: 2, height: "80%"}} >
-        <form>
-          <Grid container spacing={7}>
-            <Grid item xs={12} sx={{ marginTop: 4.8, marginBottom: 3 }}>
-              <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <ImgStyled src={img} alt='Profile Pic' />
-                <Box>
-                  <ButtonStyled component='label' variant='contained' htmlFor='account-settings-upload-image'>
-                    Upload New Photo
-                    <input
-                      hidden
-                      type='file'
-                      onChange={onChange}
-                      accept='image/png, image/jpeg'
-                      id='account-settings-upload-image'
-                    />
-                  </ButtonStyled>
-                  <ResetButtonStyled color='error' variant='outlined'>
-                    Reset
-                  </ResetButtonStyled>
-                  <Typography variant='body2' sx={{ marginTop: 5 }}>
+  return (
+    <CardContent sx={{ width: "80%", mx: "auto", p: 2, height: "80%" }}>
+      <form>
+        <Grid container spacing={7}>
+          <Grid item xs={12} sx={{ marginTop: 4.8, marginBottom: 3 }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <ImgStyled src={img} alt="Profile Pic" />
+              <Box>
+                <ButtonStyled
+                  component="label"
+                  variant="contained"
+                  htmlFor="account-settings-upload-image"
+                >
+                  Upload New Photo
+                  <input
+                    hidden
+                    type="file"
+                    onChange={onChange}
+                    accept="image/png, image/jpeg"
+                    id="account-settings-upload-image"
+                  />
+                </ButtonStyled>
+                <ResetButtonStyled color="error" variant="outlined">
+                  Reset
+                </ResetButtonStyled>
+                {/* <Typography variant='body2' sx={{ marginTop: 5 }}>
                     Allowed PNG or JPEG. Max size of 800K.
-                  </Typography>
-                </Box>
+                  </Typography> */}
               </Box>
-            </Grid>
-  
-            <Grid item xs={12} sm={6}>
-              <TextField fullWidth label='Username' placeholder='johnDoe' defaultValue='johnDoe' />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField fullWidth label='Name' placeholder='John Doe' defaultValue='John Doe' />
-            </Grid>
-            <Grid item xs={12} sm={6}>
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
               <TextField
                 fullWidth
-                type='email'
-                label='Email'
-                placeholder='johnDoe@example.com'
-                defaultValue='johnDoe@example.com'
+                label="First Name"
+                value={data.firstname}
               />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <FormControl fullWidth>
-                <InputLabel>Role</InputLabel>
-                <Select label='Role' defaultValue='admin'>
-                  <MenuItem value='admin'>Admin</MenuItem>
-                  <MenuItem value='author'>Author</MenuItem>
-                  <MenuItem value='editor'>Editor</MenuItem>
-                  <MenuItem value='maintainer'>Maintainer</MenuItem>
-                  <MenuItem value='subscriber'>Subscriber</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <FormControl fullWidth>
-                <InputLabel>Status</InputLabel>
-                <Select label='Status' defaultValue='active'>
-                  <MenuItem value='active'>Active</MenuItem>
-                  <MenuItem value='inactive'>Inactive</MenuItem>
-                  <MenuItem value='pending'>Pending</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField fullWidth label='Company' placeholder='ABC Pvt. Ltd.' defaultValue='ABC Pvt. Ltd.' />
-            </Grid>
-  
-            {openAlert ? (
-              <Grid item xs={12} sx={{ mb: 3 }}>
-                <Alert
-                  severity='warning'
-                  sx={{ '& a': { fontWeight: 400 } }}
-                  action={
-                    <IconButton size='small' color='inherit' aria-label='close' onClick={() => setOpenAlert(false)}>
-                      <CloseIcon/>
-                    </IconButton>
-                  }
-                >
-                  <AlertTitle>Your email is not confirmed. Please check your inbox.</AlertTitle>
-                  <Link href='/' onClick={e => e.preventDefault()}>
-                    Resend Confirmation
-                  </Link>
-                </Alert>
-              </Grid>
-            ) : null}
-  
-            <Grid item xs={12}>
-              <Button variant='contained' color='secondary' sx={{ marginRight: 3.5 }}>
-                Save Changes
-              </Button>
-              <Button type='reset' variant='outlined' color='secondary'>
-                Reset
-              </Button>
-            </Grid>
+            </FormControl>
           </Grid>
-        </form>
-      </CardContent>
-    );
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <TextField
+                fullWidth
+                label="Last Name"
+                value={data.lastname}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <TextField
+                fullWidth
+                label="Tên người dùng"
+                placeholder="johnDoe"
+                defaultValue="johnDoe"
+                value={data.username}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <TextField
+                fullWidth
+                label="Password"
+                type="password"
+                defaultValue=" "
+                value={data.password}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              type="email"
+              label="Email"
+              placeholder="johnDoe@example.com"
+              defaultValue="johnDoe@example.com"
+              value={data.email}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <TextField
+                label="Role"
+                defaultValue=""
+                value={data.access}
+                disabled
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel>Status</InputLabel>
+              <Select label="Status" defaultValue="active">
+                <MenuItem value="active">Active</MenuItem>
+                <MenuItem value="inactive">Inactive</MenuItem>
+                <MenuItem value="pending">Pending</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label="Contact"
+              value={data.contact}
+            />
+          </Grid>
+
+          {openAlert ? (
+            <Grid item xs={12} sx={{ mb: 3 }}>
+              <Alert
+                severity="warning"
+                sx={{ "& a": { fontWeight: 400 } }}
+                action={
+                  <IconButton
+                    size="small"
+                    color="inherit"
+                    aria-label="close"
+                    onClick={() => setOpenAlert(false)}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                }
+              >
+                <AlertTitle>
+                  Your email is not confirmed. Please check your inbox.
+                </AlertTitle>
+                <Link href="/" onClick={(e) => e.preventDefault()}>
+                  Resend Confirmation
+                </Link>
+              </Alert>
+            </Grid>
+          ) : null}
+
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{ marginRight: 3.5 }}
+            >
+              Save Changes
+            </Button>
+            <Button type="reset" variant="outlined" color="secondary">
+              Reset
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </CardContent>
+  );
 };
 
 export default UserProfile;
