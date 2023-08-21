84aaa5968976a391dd746a10a566dcecbd9b8936
Kriggerino <76442251+Kriggerino@users.noreply.github.com>
2023-08-21 20:30:49 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Topbar from "./scenes/global/Topbar";
